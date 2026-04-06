38bc63defb26b1a32b478844185b0e766ad1b129
alizayn241 <alizayn.0e9@gmail.com>
2026-04-06 23:30:02 +0200

    Isolate notifications feature UI in separate branch

--- lib/features/auth/domain/usecases/get_saved_token_usecase.dart
+++
@@ -1,8 +0,0 @@
-import '../repositories/auth_repository.dart';
-
-class GetSavedTokenUseCase {
-  final AuthRepository repo;
-  GetSavedTokenUseCase(this.repo);
-
-  Future<String?> call() => repo.getSavedToken();
-}
